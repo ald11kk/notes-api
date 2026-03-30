5c09027987eb4fd3143779d31374750791c8b73c
ald11kk <221241@astanait.edu.kz>
2026-03-30 22:11:52 +0500

    add schemas

---
+++ app/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import Optional
+
+# Общие поля которые есть при создании и при чтении
+class NoteBase(BaseModel):
+    title: str
+    content: str
+
+# Эту схему мы используем, когда КЛИЕНТ присылает нам данные (POST-запрос)
+class NoteCreate(NoteBase):
+    pass
+
+# Эту схему мы используем, когда ОТПРАВЛЯЕМ данные клиенту (Response)
+class Note(NoteBase):
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
